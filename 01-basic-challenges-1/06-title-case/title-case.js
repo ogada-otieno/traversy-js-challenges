683799d76464167f78cb196a3ad8b6345b942765
Rodgers Ogada <ogadarodgers@gmail.com>
2024-02-16 17:49:58 +0300

    passes test: titleCase takes in a string and returns the string with the first letter of each word capitalized.

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,9 @@
-function titleCase() {}
+function titleCase(str) {
+  let arr = str.toLowerCase().split(' ');
+  for (let index = 0; index < arr.length; index++) {
+    arr[index] = arr[index][0].toUpperCase() + arr[index].slice(1);
+  }
+  return arr.join(' ');
+}
 
 module.exports = titleCase;
