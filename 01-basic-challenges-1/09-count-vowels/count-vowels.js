5565b212114a64d5c43b77ac5284835b47895790
Rodgers Ogada <ogadarodgers@gmail.com>
2024-02-16 19:39:15 +0300

    passes tests: countVowels takes in a string and returns the number of vowels in the string.

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,22 @@
-function countVowels() {}
+function countVowels(str) {
+  let count = 0;
+
+  for (let index = 0; index < str.length; index++) {
+    vowels = ['a', 'e', 'i', 'o', 'u'];
+
+    const letter = str[index].toLowerCase();
+
+    for (let index = 0; index < vowels.length; index++) {
+      const vowel = vowels[index];
+
+      if (letter === vowel) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+countVowels('Flower');
 
 module.exports = countVowels;
