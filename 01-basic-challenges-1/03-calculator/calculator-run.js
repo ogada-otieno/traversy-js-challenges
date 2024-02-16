877337e5dca1b8ecc9e2da1b642023c879572383
Rodgers Ogada <ogadarodgers@gmail.com>
2024-02-16 16:04:44 +0300

    passed test: returns a number based on the result of the params or returns an error if invalid operator is passed.

--- 01-basic-challenges-1/03-calculator/calculator-run.js
+++ 01-basic-challenges-1/03-calculator/calculator-run.js
@@ -1,5 +1,9 @@
 const calculator = require('./calculator');
 
-const result = calculator(1, 2, '+');
+const result = calculator(2, 3, '*/*');
+const resultTwo = calculator(2, 3, '**');
+const resultThree = calculator(2, 1, '+');
 
 console.log(result);
+console.log(resultTwo);
+console.log(resultThree);
