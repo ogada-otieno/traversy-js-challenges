0fa9ceefe764557bd6b9f6ff15f49e48fb69c3c0
Rodgers Ogada <ogadarodgers@gmail.com>
2024-02-16 17:16:43 +0300

    passes test: findMaxNumber takes in an array of numbers and returns the largest number in the array.

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,12 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  let largestNum = arr[0];
+  for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    if (element > largestNum) {
+      largestNum = element;
+    }
+  }
+  return largestNum;
+}
 
 module.exports = findMaxNumber;
