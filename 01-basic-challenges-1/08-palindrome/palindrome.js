007b0ab78849d10df99adc4b6df5e89f663ff4ea
Rodgers Ogada <ogadarodgers@gmail.com>
2024-02-16 19:01:16 +0300

    passes tests: isPalindrome takes in a string and returns true if the string is a palindrome and false if it is not.

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,16 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  let removeNonAlphanumeric = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  let reversed = '';
+
+  for (let index = removeNonAlphanumeric.length - 1; index >= 0; index--) {
+    reversed += removeNonAlphanumeric[index];
+  }
+
+  if (reversed === removeNonAlphanumeric) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = isPalindrome;
