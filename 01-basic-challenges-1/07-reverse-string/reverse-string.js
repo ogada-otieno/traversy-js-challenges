9d7e3ab99c2e2733289712e9815d08bd27b8ffc9
Rodgers Ogada <ogadarodgers@gmail.com>
2024-02-16 18:32:32 +0300

    passes tests: reverseString  takes in a string and returns the reverse of that string.

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,11 @@
-function reverseString() {}
+function reverseString(str) {
+  let reversedStr = '';
+  for (let index = str.length - 1; index >= 0; index--) {
+    reversedStr += str[index];
+  }
+  return reversedStr;
+}
+
+//   return str.split('').reverse().join('');
 
 module.exports = reverseString;
