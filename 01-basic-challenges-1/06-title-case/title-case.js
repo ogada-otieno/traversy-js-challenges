6b25b71c73193757c65e902ac0dc4ffa36d791ed
Rodgers Ogada <ogadarodgers@gmail.com>
2024-02-16 17:58:16 +0300

    alternative solution for titleCase

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,9 +1,14 @@
 function titleCase(str) {
-  let arr = str.toLowerCase().split(' ');
-  for (let index = 0; index < arr.length; index++) {
-    arr[index] = arr[index][0].toUpperCase() + arr[index].slice(1);
-  }
-  return arr.join(' ');
+  //   let arr = str.toLowerCase().split(' ');
+  //   for (let index = 0; index < arr.length; index++) {
+  //     arr[index] = arr[index][0].toUpperCase() + arr[index].slice(1);
+  //   }
+  //     return arr.join(' ');
+  return str
+    .toLowerCase()
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
 }
 
 module.exports = titleCase;
