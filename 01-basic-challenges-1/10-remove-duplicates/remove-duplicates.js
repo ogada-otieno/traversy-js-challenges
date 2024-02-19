8f688a07bc3308c1235253edcc9736f4c0bddb10
Rodgers Ogada <ogadarodgers@gmail.com>
2024-02-19 08:01:40 +0300

    passes tests: removeDuplicates that takes in an array and returns a new array with duplicates removed.

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,12 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+  //   let arrWithoutDup = [];
+  //   for (let index = 0; index < arr.length; index++) {
+  //     if (!arrWithoutDup.includes(arr[index])) {
+  //       arrWithoutDup.push(arr[index]);
+  //     }
+  //   }
+  //   return arrWithoutDup;
+  return Array.from(new Set(arr));
+}
 
 module.exports = removeDuplicates;
