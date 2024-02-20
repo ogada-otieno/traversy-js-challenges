251a395e4a44080bed68db7d964ae1c6310e5bad
Rodgers Ogada <ogadarodgers@gmail.com>
2024-02-20 11:28:45 +0300

    passes tests: arrayIntersection  takes in two arrays and returns an array containing the intersection of the two input arrays

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,14 @@
-function arrayIntersection() {}
-
+function arrayIntersection(arr1, arr2) {
+  // initialize empty array to store intersection
+  let arr3 = [];
+  // loop over arr1
+  for (let index = 0; index < arr1.length; index++) {
+    const element = arr1[index];
+    // use includes() to check if elements of arr1 are present in arr2 and push the intersection values into arr3
+    if (arr2.includes(element)) {
+      arr3.push(element);
+    }
+  }
+  return arr3;
+}
 module.exports = arrayIntersection;
