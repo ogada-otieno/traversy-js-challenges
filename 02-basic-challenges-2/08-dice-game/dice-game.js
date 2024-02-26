47b52807322396f1ae62965bb6a5a60d25bcdc5e
Rodgers Ogada <ogadarodgers@gmail.com>
2024-02-26 11:24:07 +0300

    passes tests: diceGameSimulation simulates a dice game.

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,27 @@
-function diceGameSimulation() {}
+const rollDice = () => {
+  return Math.floor(Math.random() * 6) + 1;
+};
+
+function diceGameSimulation(numSimulations) {
+  const results = [];
+
+  for (let index = 0; index < numSimulations; index++) {
+    let dice1 = rollDice();
+    let dice2 = rollDice();
+    let sum = dice1 + dice2;
+
+    let result = '';
+
+    if (sum === 7 || sum === 11) {
+      result = 'win';
+    } else if (sum === 2 || sum === 3 || sum === 12) {
+      result = 'lose';
+    } else {
+      result = 'roll again';
+    }
+    results.push({ dice1, dice2, sum, result });
+  }
+  return results;
+}
 
 module.exports = diceGameSimulation;
