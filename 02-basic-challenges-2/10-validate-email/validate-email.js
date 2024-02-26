d8e5e0fbec719c0ec64bbbc283f43310b83b5b9d
Rodgers Ogada <ogadarodgers@gmail.com>
2024-02-26 12:19:19 +0300

    passes tests: validateEmail takes in a string and returns whether the string is a valid email address. For the purposes of this challenge, a valid email address is defined as a string that contains an @ symbol and a . symbol.

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,27 @@
-function validateEmail() {}
+function validateEmail(email) {
+  //   const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+  //   return emailRegex.test(email);
+  if (email.indexOf('@') === -1) {
+    console.log(false);
+    return false;
+  }
+
+  const [local, domain] = email.split('@');
+  console.log(local, domain);
+
+  if (local.length === 0 || local.length < 3) {
+    return false;
+  }
+
+  const domainExtension = domain.split('.');
+
+  if (
+    domainExtension.length < 2 ||
+    domainExtension[domainExtension.length - 1].length < 2
+  ) {
+    return false;
+  }
+  return true;
+}
 
 module.exports = validateEmail;
