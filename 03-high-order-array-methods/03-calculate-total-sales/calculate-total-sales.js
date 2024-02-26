285efc81f2f915d3780cd66712809ec4a457aaef
Rodgers Ogada <ogadarodgers@gmail.com>
2024-02-26 22:32:00 +0300

    passes tests: calculateTotalSalesWithTax takes in an array of product objects, along with the tax rate, and returns the total sales amount including tax.

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,13 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+  let totalSalesWithoutTax = products.reduce(
+    (total, product) => total + product.price * product.quantity,
+    0,
+  );
+  console.log(totalSalesWithoutTax);
+  let taxAmount = (totalSalesWithoutTax * taxRate) / 100;
+  let totalSalesWithTax = totalSalesWithoutTax + taxAmount;
+  console.log(totalSalesWithTax);
+  return totalSalesWithTax;
+}
 
 module.exports = calculateTotalSalesWithTax;
