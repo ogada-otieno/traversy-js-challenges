322a6f30327dbbaee449d83dea68523b6ad938e8
Rodgers Ogada <ogadarodgers@gmail.com>
2024-02-26 14:26:40 +0300

    passes tests: sumOfEvenSquares takes an array of numbers and returns the sum of the squares of the even numbers in the array.

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,11 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(arr) {
+  const totalSum = arr
+    .filter((num) => num % 2 == 0)
+    .map((num) => num ** 2)
+    .reduce((total, num) => total + num, 0);
+  return totalSum;
+}
+
+sumOfEvenSquares([1, 2, 3, 4, 5]);
 
 module.exports = sumOfEvenSquares;
