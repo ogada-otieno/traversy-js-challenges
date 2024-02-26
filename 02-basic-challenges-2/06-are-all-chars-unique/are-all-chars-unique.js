449a72303025516d68ab4afc2de62082d54c1517
Rodgers Ogada <ogadarodgers@gmail.com>
2024-02-26 09:33:22 +0300

    passes tests: areAllCharactersUnique takes in a string and returns true or false depending on whether all characters in the string are unique (i.e., no character is repeated).

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,18 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  // initialize the new Set
+  let charSet = new Set();
+  // iterate over the str and check if there are any repeating characters
+  for (let index = 0; index < str.length; index++) {
+    let char = str[index];
+    if (charSet.has(char)) {
+      // if we've gone through the string and encountered the char already, return false
+      return false;
+    }
+    //   if the char hasn't been encountered, add it to our Set
+    charSet.add(char);
+  }
+  // then return true
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
