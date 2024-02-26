68a146bb5d4aaec2461ed24f36523e130ee0bfc2
Rodgers Ogada <ogadarodgers@gmail.com>
2024-02-26 07:28:50 +0300

    passes tests: findMissingLetter  takes in an array of consecutive (increasing) letters as input and returns the missing letter in the array.

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,19 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  const alphabetArr = alphabet.split('');
+  let indexFirst = alphabetArr.indexOf(arr[0]);
+  let indexLast = alphabetArr.indexOf(arr[arr.length - 1]);
+  let cut = alphabetArr.slice(indexFirst, indexLast + 1);
+
+  for (let index = 0; index < cut.length; index++) {
+    const element = cut[index];
+    if (!arr.includes(element)) {
+      return element;
+    }
+  }
+  return '';
+}
+
 
 module.exports = findMissingLetter;
