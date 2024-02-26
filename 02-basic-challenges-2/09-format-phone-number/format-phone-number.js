f379f672b9d61844d972070506ffb3b4b84fa739
Rodgers Ogada <ogadarodgers@gmail.com>
2024-02-26 12:02:03 +0300

    passes tests: formatPhoneNumber takes in an array of numbers and returns a string representing the phone number formed by concatenating the numbers in the specified format.

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,11 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(arr) {
+  let firstThree = arr.slice(0, 3).join('');
+  let secondThree = arr.slice(3, 6).join('');
+  let lastDigits = arr.slice(6, 10).join('');
+  let phone = `(${firstThree}) ${secondThree}-${lastDigits}`;
+  return phone;
+}
+
+formatPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]); // => "(123) 456-7890"
 
 module.exports = formatPhoneNumber;
