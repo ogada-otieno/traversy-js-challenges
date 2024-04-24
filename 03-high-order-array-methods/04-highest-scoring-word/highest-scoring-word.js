cd2207285e24ba3af73ea42341a878c00097fbe2
Rodgers Ogada <ogadarodgers@gmail.com>
2024-04-24 23:11:19 +0300

    highest scoring word

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,26 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+  let words = str.split(' ');
+
+  let scores = words.map((word) => {
+    let score = 0;
+    for (const letter of word) {
+      score += letter.charCodeAt(0) - 96;
+    }
+    return score;
+  });
+
+  let highestScore = 0;
+  highestIndex = 0;
+  for (let i = 0; i < scores.length; i++) {
+    if (scores[i] > highestScore) {
+      highestScore = scores[i];
+      highestIndex = i;
+    }
+  }
+
+  return words[highestIndex];
+}
+
+highestScoringWord('man i need a taxi up to ubud'); // 'taxi'
 
 module.exports = highestScoringWord;
